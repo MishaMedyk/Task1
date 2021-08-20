80ee888d4d6bc97aeb34708ee440c00ede23ce2d
Misha Medyk <misha.medyk2000@gmail.com>
2021-08-20 11:47:16 +0300

    Task 1,6

---
+++ src/javaClub/team3/Task1.java
@@ -0,0 +1,7 @@
+package javaClub.team3;
+
+public class Task1 {
+    public static int doubleInteger(int i) {
+        return i*2;
+    }
+}
---
+++ src/javaClub/team3/Task6.java
@@ -0,0 +1,7 @@
+package javaClub.team3;
+
+public class Task6 {
+    public static int ToBinary(int n){
+        return Integer.valueOf(Integer.toBinaryString(n));
+    }
+}
